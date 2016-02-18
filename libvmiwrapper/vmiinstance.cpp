831b8921ab7a53eeff6e7dde40727dc6aa26d214
Jonas Jelten <jj@sft.mx>
2016-02-18 18:08:28 +0100

    beautified vmiinstance

--- libvmiwrapper/vmiinstance.cpp
+++ libvmiwrapper/vmiinstance.cpp
@@ -16,16 +16,16 @@
 
 #include "helpers.h"
 
-#define IA32E_IS_PAGE_USER_SUPERVISOR(page_info) \
-  	(USER_SUPERVISOR(page_info->x86_ia32e.pte_value) && \
- 	 USER_SUPERVISOR(page_info->x86_ia32e.pgd_value) && \
+#define IA32E_IS_PAGE_USER_SUPERVISOR(page_info)        \
+	(USER_SUPERVISOR(page_info->x86_ia32e.pte_value) && \
+	 USER_SUPERVISOR(page_info->x86_ia32e.pgd_value) && \
 	 USER_SUPERVISOR(page_info->x86_ia32e.pdpte_value) && \
 	 USER_SUPERVISOR(page_info->x86_ia32e.pml4e_value))
 
-#define IA32E_IS_PAGE_SUPERVISOR(page_info)        (!(IA32E_IS_PAGE_USER_SUPERVISOR(page_info)))
+#define IA32E_IS_PAGE_SUPERVISOR(page_info) (!(IA32E_IS_PAGE_USER_SUPERVISOR(page_info)))
 
 #define PAGE_NX(entry)	        VMI_GET_BIT(entry, 63)
-#define IS_PAGE_NX(page_info) \
+#define IS_PAGE_NX(page_info)                   \
 	(PAGE_NX(page_info->x86_ia32e.pte_value) || \
 	 PAGE_NX(page_info->x86_ia32e.pgd_value) || \
 	 PAGE_NX(page_info->x86_ia32e.pdpte_value) || \
@@ -37,108 +37,115 @@
 
 VMIInstance *VMIInstance::instance = NULL;
 
-VMIInstance::VMIInstance(std::string name, uint32_t flags):
+VMIInstance::VMIInstance(std::string name, uint32_t flags)
+	:
 	vmiMutex(),
-	paused(false){
-	
-	if(!flags){
+	paused(false) {
+
+	if (!flags) {
 		flags = VMI_AUTO | VMI_INIT_COMPLETE;
 	}
 
-    /* initialize the libvmi library */
-	char * name_i = const_cast<char*>(name.c_str());
-    if (vmi_init(&vmi, flags, name_i) == VMI_FAILURE) {
-        printf("Failed to init LibVMI library.\n");
-        throw VMIException();
-    }
+	/* initialize the libvmi library */
+	char *name_i = const_cast<char *>(name.c_str());
+	if (vmi_init(&vmi, flags, name_i) == VMI_FAILURE) {
+		printf("Failed to init LibVMI library.\n");
+		throw VMIException();
+	}
 
-    /* init the offset values */
-    if (VMI_OS_LINUX != vmi_get_ostype(vmi)) {
-        printf("This tool only supports Linux\n");
-        throw VMIException();
-    }
+	/* init the offset values */
+	if (VMI_OS_LINUX != vmi_get_ostype(vmi)) {
+		printf("This tool only supports Linux\n");
+		throw VMIException();
+	}
 
 	instance = this;
 	return;
 }
 
-VMIInstance::~VMIInstance(){
-    /* resume the vm */
-    this->resumeVM();
+VMIInstance::~VMIInstance() {
+	/* resume the vm */
+	this->resumeVM();
 
 	instance = NULL;
 
-    /* cleanup any memory associated with the LibVMI instance */
-    vmi_destroy(vmi);
+	/* cleanup any memory associated with the LibVMI instance */
+	vmi_destroy(vmi);
 	return;
 }
 
-VMIInstance *VMIInstance::getInstance(){
+VMIInstance *VMIInstance::getInstance() {
 	return VMIInstance::instance;
 }
 
-void VMIInstance::pauseVM(){
-    /* pause the vm for consistent memory access */
-    if (vmi_pause_vm(vmi) != VMI_SUCCESS) {
-        printf("Failed to pause VM\n");
-        throw VMIException();
-    }
+void VMIInstance::pauseVM() {
+	/* pause the vm for consistent memory access */
+	if (vmi_pause_vm(vmi) != VMI_SUCCESS) {
+		printf("Failed to pause VM\n");
+		throw VMIException();
+	}
 	paused = true;
 }
 
-void VMIInstance::resumeVM(){
-    /* pause the vm for consistent memory access */
-    if (!paused) return;
+void VMIInstance::resumeVM() {
+	/* pause the vm for consistent memory access */
+	if (!paused)
+		return;
 	if (vmi_resume_vm(vmi) != VMI_SUCCESS) {
-        printf("Failed to resume VM\n");
-        throw VMIException();
-    }
+		printf("Failed to resume VM\n");
+		throw VMIException();
+	}
 	paused = false;
 }
 
-vmi_instance_t VMIInstance::getLibVMIInstance(){
+vmi_instance_t VMIInstance::getLibVMIInstance() {
 	return vmi;
 }
 
-void VMIInstance::printKernelPages() const{
+void VMIInstance::printKernelPages() const {
+	addr_t init_dtb = vmi_pid_to_dtb(vmi, 1);
 
-    addr_t init_dtb = vmi_pid_to_dtb(vmi, 1);
+	printf("Init dtb is at %p\n", (void *)init_dtb);
 
-    printf("Init dtb is at %p\n", (void*) init_dtb);
+	GSList *pages = vmi_get_va_pages(vmi, init_dtb);
 
-    GSList* pages = vmi_get_va_pages(vmi, init_dtb);
+	if (pages != NULL)
+		printf("Got Pages\n");
 
-    if(pages != NULL) printf("Got Pages\n");
+	page_info_t *item;
 
-    page_info_t *item;
-
-    for(GSList *__glist = pages; __glist ; __glist = __glist->next){
-	    item = (page_info_t*) __glist->data;
-	    if (item->x86_ia32e.pml4e_value == 0){
-            printf("l4_v is null\n");
-    	}else {
-			//if(ENTRY_PRESENT(item->x86_ia32e.pte_value, VMI_OS_LINUX) && IA32E_IS_PAGE_SUPERVISOR(item)){
-			if(ENTRY_PRESENT(item->x86_ia32e.pte_value, VMI_OS_LINUX) && 
-					item->vaddr >> 40 != 0x88){
-				if(IA32E_IS_PAGE_USER_SUPERVISOR(item)) continue;
-		        printf("Page at %16lx : %016lx - %s, %s ", item->vaddr, item->paddr, (IA32E_IS_PAGE_SUPERVISOR(item)) ? "S": "U" , (IS_PAGE_NX(item)) ? "NX" : "X");
+	for (GSList *__glist = pages; __glist; __glist = __glist->next) {
+		item = (page_info_t *)__glist->data;
+		if (item->x86_ia32e.pml4e_value == 0) {
+			printf("l4_v is null\n");
+		} else {
+			// if(ENTRY_PRESENT(item->x86_ia32e.pte_value, VMI_OS_LINUX) &&
+			// IA32E_IS_PAGE_SUPERVISOR(item)){
+			if (ENTRY_PRESENT(item->x86_ia32e.pte_value, VMI_OS_LINUX) &&
+			    item->vaddr >> 40 != 0x88) {
+				if (IA32E_IS_PAGE_USER_SUPERVISOR(item))
+					continue;
+				printf("Page at %16lx : %016lx - %s, %s ",
+				       item->vaddr,
+				       item->paddr,
+				       (IA32E_IS_PAGE_SUPERVISOR(item)) ? "S" : "U",
+				       (IS_PAGE_NX(item)) ? "NX" : "X");
 				printf("Page size: %i\n", item->size);
-				
-				//printf("Settings: %i - %i - %i - %i\n", 
+
+				// printf("Settings: %i - %i - %i - %i\n",
 				//    USER_SUPERVISOR(item->x86_ia32e.pte_value),
 				//    USER_SUPERVISOR(item->x86_ia32e.pgd_value),
 				//    USER_SUPERVISOR(item->x86_ia32e.pdpte_value),
 				//    USER_SUPERVISOR(item->x86_ia32e.pml4e_value));
-				
 			}
-        }
+		}
 		free(item);
-    }
+	}
 	free(pages);
 }
 
-PageMap VMIInstance::destroyMap(PageMap map) const{
-	for (auto& item : map){
+PageMap VMIInstance::destroyMap(PageMap map) const {
+	for (auto &item : map) {
 		free(item.second);
 	}
 	map.clear();
@@ -147,39 +154,40 @@ PageMap VMIInstance::destroyMap(PageMap map) const{
 
 PageMap VMIInstance::getPages(uint32_t pid, bool executable) {
 	vmiMutex.lock();
-	addr_t init_dtb = vmi_pid_to_dtb(vmi, ((pid == 0)? 1: pid));
-    GSList* pages = vmi_get_va_pages(vmi, init_dtb);
+	addr_t init_dtb = vmi_pid_to_dtb(vmi, ((pid == 0) ? 1 : pid));
+	GSList *pages = vmi_get_va_pages(vmi, init_dtb);
 	vmiMutex.unlock();
-    assert(pages);
+	assert(pages);
 
-    page_info_t *item;
+	page_info_t *item;
 	PageMap pageMap;
 
-    for(GSList *__glist = pages; __glist ; __glist = __glist->next){
-	    item = (page_info_t*) __glist->data;
-	    if (item->x86_ia32e.pml4e_value != 0 &&
-		    ENTRY_PRESENT(item->x86_ia32e.pte_value, VMI_OS_LINUX) && 
-		    ((pid == 0 &&  IA32E_IS_PAGE_SUPERVISOR(item)) ||
-			 (pid != 0 && !IA32E_IS_PAGE_SUPERVISOR(item))) &&
-		    (!executable || !IS_PAGE_NX(item))){
-			pageMap.insert(std::pair<uint64_t,page_info_t *>(item->vaddr, item));
-		}else{
+	for (GSList *__glist = pages; __glist; __glist = __glist->next) {
+		item = (page_info_t *)__glist->data;
+		if (item->x86_ia32e.pml4e_value != 0 &&
+		    ENTRY_PRESENT(item->x86_ia32e.pte_value, VMI_OS_LINUX) &&
+		    ((pid == 0 && IA32E_IS_PAGE_SUPERVISOR(item)) ||
+		     (pid != 0 && !IA32E_IS_PAGE_SUPERVISOR(item))) &&
+		    (!executable || !IS_PAGE_NX(item))) {
+			pageMap.insert(
+			    std::pair<uint64_t, page_info_t *>(item->vaddr, item));
+		} else {
 			free(item);
 		}
-    }
+	}
 	g_slist_free(pages);
 	return pageMap;
 }
 
-uint64_t VMIInstance::translateV2P(uint64_t va, uint32_t pid){
+uint64_t VMIInstance::translateV2P(uint64_t va, uint32_t pid) {
 	std::lock_guard<std::mutex> lock(vmiMutex);
-	if(pid){
+	if (pid) {
 		return vmi_translate_uv2p(vmi, va, pid);
 	}
 	return vmi_translate_kv2p(vmi, va);
 }
 
-uint8_t VMIInstance::read8FromVA(uint64_t va, uint32_t pid){
+uint8_t VMIInstance::read8FromVA(uint64_t va, uint32_t pid) {
 	uint8_t value = 0;
 	vmiMutex.lock();
 	vmi_read_8_va(vmi, va, pid, &value);
@@ -187,7 +195,7 @@ uint8_t VMIInstance::read8FromVA(uint64_t va, uint32_t pid){
 	return value;
 }
 
-uint16_t VMIInstance::read16FromVA(uint64_t va, uint32_t pid){
+uint16_t VMIInstance::read16FromVA(uint64_t va, uint32_t pid) {
 	uint16_t value = 0;
 	vmiMutex.lock();
 	vmi_read_16_va(vmi, va, pid, &value);
@@ -195,7 +203,7 @@ uint16_t VMIInstance::read16FromVA(uint64_t va, uint32_t pid){
 	return value;
 }
 
-uint32_t VMIInstance::read32FromVA(uint64_t va, uint32_t pid){
+uint32_t VMIInstance::read32FromVA(uint64_t va, uint32_t pid) {
 	uint32_t value = 0;
 	vmiMutex.lock();
 	vmi_read_32_va(vmi, va, pid, &value);
@@ -203,7 +211,7 @@ uint32_t VMIInstance::read32FromVA(uint64_t va, uint32_t pid){
 	return value;
 }
 
-uint64_t VMIInstance::read64FromVA(uint64_t va, uint32_t pid){
+uint64_t VMIInstance::read64FromVA(uint64_t va, uint32_t pid) {
 	uint64_t value = 0;
 	vmiMutex.lock();
 	vmi_read_64_va(vmi, va, pid, &value);
@@ -211,27 +219,30 @@ uint64_t VMIInstance::read64FromVA(uint64_t va, uint32_t pid){
 	return value;
 }
 
-std::vector<uint8_t> 
-    VMIInstance::readVectorFromVA(uint64_t va, uint64_t len,
-                                  uint32_t pid, bool fillunmapped){
-	uint8_t* buffer = (uint8_t*) malloc(len);
+std::vector<uint8_t> VMIInstance::readVectorFromVA(uint64_t va,
+                                                   uint64_t len,
+                                                   uint32_t pid,
+                                                   bool fillunmapped) {
+	uint8_t *buffer = (uint8_t *)malloc(len);
 	memset(buffer, 0, len);
 	std::vector<uint8_t> result;
 	size_t size = 0;
-	size_t res = 0;
+	size_t res  = 0;
 
-	while(size < len){
+	while (size < len) {
 		vmiMutex.lock();
-		res = vmi_read_va(vmi, va + size, pid,
-		    buffer + size,
-		    std::min(0x1000, (int)(len - size)));
+		res = vmi_read_va(vmi,
+		                  va + size,
+		                  pid,
+		                  buffer + size,
+		                  std::min(0x1000, (int)(len - size)));
 		vmiMutex.unlock();
 		size += res;
-		if(res == 0){
-			if (fillunmapped){
+		if (res == 0) {
+			if (fillunmapped) {
 				// Try to read next page
 				size += 0x1000;
-			}else{
+			} else {
 				// Unable to return more, probably not mapped
 				break;
 			}
@@ -242,12 +253,11 @@ std::vector<uint8_t>
 
 	free(buffer);
 	return result;
-
 }
 
-std::string VMIInstance::readStrFromVA(uint64_t va, uint32_t pid){
+std::string VMIInstance::readStrFromVA(uint64_t va, uint32_t pid) {
 	vmiMutex.lock();
-	char * str = vmi_read_str_va(vmi, va, pid);
+	char *str = vmi_read_str_va(vmi, va, pid);
 	vmiMutex.unlock();
 
 	assert(str);
@@ -256,26 +266,26 @@ std::string VMIInstance::readStrFromVA(uint64_t va, uint32_t pid){
 	return result;
 }
 
-bool VMIInstance::isPageSuperuser(page_info_t* page) const{
-    bool ret = IA32E_IS_PAGE_SUPERVISOR(page);
+bool VMIInstance::isPageSuperuser(page_info_t *page) const {
+	bool ret = IA32E_IS_PAGE_SUPERVISOR(page);
 	return ret;
 }
 
-bool VMIInstance::isPageExecutable(page_info_t* page) const{
+bool VMIInstance::isPageExecutable(page_info_t *page) const {
 	bool ret = !IS_PAGE_NX(page);
 	return ret;
 }
 
-void VMIInstance::dumpMemory(uint64_t address, uint64_t len, std::string filename){
+void VMIInstance::dumpMemory(uint64_t address,
+                             uint64_t len,
+                             std::string filename) {
 	vmiMutex.lock();
-	std::vector<uint8_t> dump = 
-                 this->readVectorFromVA(address, len);
+	std::vector<uint8_t> dump = this->readVectorFromVA(address, len);
 	vmiMutex.unlock();
 
-	std::ofstream file (filename, std::ios::binary);
-	if (file.is_open())
-	{
-		file.write((char*) dump.data(), dump.size());
+	std::ofstream file(filename, std::ios::binary);
+	if (file.is_open()) {
+		file.write((char *)dump.data(), dump.size());
 		file.close();
 	}
 	return;
